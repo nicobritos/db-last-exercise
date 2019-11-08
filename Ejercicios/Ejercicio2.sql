92c9ab57f3cf4e0ac1285b6ab906e101529a5103
nicbritos <nicbritos@hotmail.com>
2019-11-07 21:04:39 -0300

    Moved item "a" and "b" to "Ejercicio2.sql"

--- Ejercicios/Ejercicio2.sql
+++ Ejercicios/Ejercicio2.sql
@@ -1,3 +1,8 @@
+DROP FUNCTION IF EXISTS get_n_old_passwords CASCADE;
+DROP FUNCTION IF EXISTS repeats_password CASCADE;
+DROP FUNCTION IF EXISTS validacion_contrasena_funcion CASCADE;
+DROP TRIGGER IF EXISTS validacion_contrasena_trigger ON USUARIO CASCADE;
+
 DROP TABLE IF EXISTS USUARIO CASCADE;
 DROP TABLE IF EXISTS ROL CASCADE;
 DROP TABLE IF EXISTS ROLES CASCADE;
@@ -59,3 +64,57 @@ INSERT INTO HISTORIALPASSWORD (Usuario, Password, Fecha) VALUES ('tbalbin', 'pas
 INSERT INTO HISTORIALPASSWORD (Usuario, Password, Fecha) VALUES ('ucampos', 'pass2', '01/01/2019 00:00:00');
 INSERT INTO HISTORIALPASSWORD (Usuario, Password, Fecha) VALUES ('ucampos', 'pass3', '01/02/2019 00:00:00');
 INSERT INTO HISTORIALPASSWORD (Usuario, Password, Fecha) VALUES ('ucampos', 'pass4', '01/03/2019 00:00:00');
+
+
+-- 2.b Trigger de validacion de contrasena
+-- Funciones
+CREATE FUNCTION get_n_old_passwords(usuarioIn TEXT) RETURNS INT
+	AS $function$
+	BEGIN
+		RETURN (
+			SELECT MAX(Nivel) FROM ROL
+				INNER JOIN ROLES ON ROLES.Rol = ROL.Nombre
+				WHERE ROLES.Usuario = usuarioIn
+		);
+	END;
+	$function$
+	LANGUAGE plpgsql;
+
+CREATE FUNCTION repeats_password(usuarioIn TEXT, pw TEXT, n INT) RETURNS BOOL
+	AS $function$
+	BEGIN
+		RETURN pw IN (
+			SELECT Password FROM HISTORIALPASSWORD
+				WHERE Usuario = usuarioIn
+				ORDER BY Fecha DESC
+				LIMIT n
+		);
+	END;
+	$function$
+	LANGUAGE plpgsql;
+
+CREATE FUNCTION validacion_contrasena_funcion() RETURNS TRIGGER
+	AS $function$
+	DECLARE
+		n INT := get_n_old_passwords(OLD.Nombre);
+	BEGIN
+		IF (OLD.Password = NEW.Password) THEN
+			RAISE EXCEPTION 'No puede repetir el password anterior';
+		END IF;
+		
+		IF (repeats_password(OLD.Nombre, NEW.Password, n)) THEN
+			RAISE EXCEPTION 'El password no debe de coincidir con ninguno de los % passwords anteriores', n;
+		END IF;
+		
+		INSERT INTO HISTORIALPASSWORD (Usuario, Password, Fecha) VALUES (OLD.Nombre, OLD.Password, NOW());
+		
+		RETURN NEW;
+	END;
+	$function$
+	LANGUAGE plpgsql;
+
+-- Trigger
+CREATE TRIGGER validacion_contrasena_trigger BEFORE UPDATE
+	ON USUARIO
+	FOR EACH ROW
+	EXECUTE PROCEDURE validacion_contrasena_funcion();
